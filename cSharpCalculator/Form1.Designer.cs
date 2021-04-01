42cdaaa9f1a468905aac718b34f45596b4aa58cc
Antal Toth <anthony.toth94@gmail.com>
2021-04-01 12:53:15 +0200

    Commit

---
+++ cSharpCalculator/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace cSharpCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonOsszead = new System.Windows.Forms.Button();
+            this.buttonElvesz = new System.Windows.Forms.Button();
+            this.buttonSzoroz = new System.Windows.Forms.Button();
+            this.buttonOszt = new System.Windows.Forms.Button();
+            this.buttonSzamol = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(34, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(223, 33);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(205, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 39);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(34, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 39);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(123, 190);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(52, 39);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(205, 190);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(52, 39);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(34, 263);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(52, 39);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(123, 263);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(52, 39);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(205, 263);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(52, 39);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(34, 331);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(52, 39);
+            this.button0.TabIndex = 1;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(123, 331);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(52, 39);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Töröl";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonOsszead
+            // 
+            this.buttonOsszead.Location = new System.Drawing.Point(294, 118);
+            this.buttonOsszead.Name = "buttonOsszead";
+            this.buttonOsszead.Size = new System.Drawing.Size(52, 39);
+            this.buttonOsszead.TabIndex = 1;
+            this.buttonOsszead.Text = "+";
+            this.buttonOsszead.UseVisualStyleBackColor = true;
+            this.buttonOsszead.Click += new System.EventHandler(this.buttonOsszead_Click);
+            // 
+            // buttonElvesz
+            // 
+            this.buttonElvesz.Location = new System.Drawing.Point(294, 190);
+            this.buttonElvesz.Name = "buttonElvesz";
+            this.buttonElvesz.Size = new System.Drawing.Size(52, 39);
+            this.buttonElvesz.TabIndex = 1;
+            this.buttonElvesz.Text = "-";
+            this.buttonElvesz.UseVisualStyleBackColor = true;
+            this.buttonElvesz.Click += new System.EventHandler(this.buttonElvesz_Click);
+            // 
+            // buttonSzoroz
+            // 
+            this.buttonSzoroz.Location = new System.Drawing.Point(294, 263);
+            this.buttonSzoroz.Name = "buttonSzoroz";
+            this.buttonSzoroz.Size = new System.Drawing.Size(52, 39);
+            this.buttonSzoroz.TabIndex = 1;
+            this.buttonSzoroz.Text = "*";
+            this.buttonSzoroz.UseVisualStyleBackColor = true;
+            this.buttonSzoroz.Click += new System.EventHandler(this.buttonSzoroz_Click);
+            // 
+            // buttonOszt
+            // 
+            this.buttonOszt.Location = new System.Drawing.Point(294, 331);
+            this.buttonOszt.Name = "buttonOszt";
+            this.buttonOszt.Size = new System.Drawing.Size(52, 39);
+            this.buttonOszt.TabIndex = 1;
+            this.buttonOszt.Text = "/";
+            this.buttonOszt.UseVisualStyleBackColor = true;
+            this.buttonOszt.Click += new System.EventHandler(this.buttonOszt_Click);
+            // 
+            // buttonSzamol
+            // 
+            this.buttonSzamol.Location = new System.Drawing.Point(205, 331);
+            this.buttonSzamol.Name = "buttonSzamol";
+            this.buttonSzamol.Size = new System.Drawing.Size(52, 39);
+            this.buttonSzamol.TabIndex = 1;
+            this.buttonSzamol.Text = "=";
+            this.buttonSzamol.UseVisualStyleBackColor = true;
+            this.buttonSzamol.Click += new System.EventHandler(this.buttonSzamol_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(275, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(44, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 24);
+            this.label2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 405);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOszt);
+            this.Controls.Add(this.buttonSzoroz);
+            this.Controls.Add(this.buttonSzamol);
+            this.Controls.Add(this.buttonElvesz);
+            this.Controls.Add(this.buttonOsszead);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Szamologep";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonOsszead;
+        private System.Windows.Forms.Button buttonElvesz;
+        private System.Windows.Forms.Button buttonSzoroz;
+        private System.Windows.Forms.Button buttonOszt;
+        private System.Windows.Forms.Button buttonSzamol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ cSharpCalculator/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharpCalculator
+{
+    public partial class Form1 : Form
+    {
+        int egy = 1;
+        int ketto = 2;
+        int harom = 3;
+        int negy = 4;
+        int ot = 5;
+        int hat = 6;
+        int het = 7;
+        int nyolc = 8;
+        int kilenc = 9;
+        int nulla = 0;
+        string plusz = "+";
+        string kivonas = "-";
+        string szoroz = "*";
+        string oszt = "/";
+        double elsoOsszeg = 0;
+        double osszeg = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += egy.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ketto.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += harom.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += negy.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ot.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += hat.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += het.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += nyolc.ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += kilenc.ToString();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += nulla.ToString();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void buttonOsszead_Click(object sender, EventArgs e)
+        {
+            elsoOsszeg = Convert.ToInt32(textBox1.Text);
+            label1.Text = plusz;
+            label2.Text = elsoOsszeg.ToString();
+            textBox1.Clear();
+        }
+
+        private void buttonSzamol_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "+")
+            {
+                osszeg = elsoOsszeg + Convert.ToDouble(textBox1.Text);
+            }
+            else if (label1.Text == "-")
+            {
+                osszeg = elsoOsszeg - Convert.ToDouble(textBox1.Text);
+            }
+            else if (label1.Text == "*")
+            {
+                osszeg = elsoOsszeg * Convert.ToDouble(textBox1.Text);
+            }
+            else if (label1.Text == "/")
+            {
+                osszeg = elsoOsszeg / Convert.ToDouble(textBox1.Text);
+            }
+            textBox1.Text = osszeg.ToString();
+            elsoOsszeg = 0;
+            label1.Text = "";
+            label2.Text = "";
+
+
+        }
+
+        private void buttonElvesz_Click(object sender, EventArgs e)
+        {
+            elsoOsszeg = Convert.ToInt32(textBox1.Text);
+            label1.Text = kivonas;
+            label2.Text = elsoOsszeg.ToString();
+            textBox1.Clear();
+        }
+
+        private void buttonSzoroz_Click(object sender, EventArgs e)
+        {
+            elsoOsszeg = Convert.ToInt32(textBox1.Text);
+            label1.Text = szoroz;
+            label2.Text = elsoOsszeg.ToString();
+            textBox1.Clear();
+        }
+
+        private void buttonOszt_Click(object sender, EventArgs e)
+        {
+            elsoOsszeg = Convert.ToDouble(textBox1.Text);
+            label1.Text = oszt;
+            label2.Text = elsoOsszeg.ToString();
+            textBox1.Clear();
+        }
+    }
+}
